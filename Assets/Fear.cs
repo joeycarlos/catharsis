d5e7c60d7db251dc4ab17a7c789cf81d949cdfe7
joeycarlos <jclaguio@gmail.com>
2020-07-08 23:22:47 -0700

    Add more enemy type prefabs

---
+++ Assets/Fear.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fear : Enemy
+{
+    public float moveSpeed;
+
+    new void Start() {
+        base.Start();
+    }
+
+    void Update() {
+        Move();
+    }
+
+    void Move() {
+        Vector3 moveVector = new Vector3(0, -moveSpeed, 0) * Time.deltaTime;
+        this.transform.position += moveVector;
+    }
+}
